58c2f3d405a611428c53f39d1c3410f1c0e6d7b4
dp <hudomokp@gmail.com>
2019-02-08 21:45:27 +0100

    Refactored model to use common base interface

--- sfg-pet-clinic-data/src/main/java/hu/atsoft/playground/sfgpetclinic/services/OwnerService.java
+++ sfg-pet-clinic-data/src/main/java/hu/atsoft/playground/sfgpetclinic/services/OwnerService.java
@@ -1,12 +1,7 @@
 package hu.atsoft.playground.sfgpetclinic.services;
 
-import java.util.Set;
-
 import hu.atsoft.playground.sfgpetclinic.model.Owner;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 	Owner findByLastName(String lastName);
-	Owner findById(Long id);
-	Owner save(Owner owner);
-	Set<Owner> findAll();
 }
--- sfg-pet-clinic-data/src/main/java/hu/atsoft/playground/sfgpetclinic/services/VetService.java
+++ sfg-pet-clinic-data/src/main/java/hu/atsoft/playground/sfgpetclinic/services/VetService.java
@@ -1,11 +1,7 @@
 package hu.atsoft.playground.sfgpetclinic.services;
 
-import java.util.Set;
-
 import hu.atsoft.playground.sfgpetclinic.model.Vet;
 
-public interface VetService {
-	Vet findById(Long id);
-	Vet save(Vet vet);
-	Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
+
 }
