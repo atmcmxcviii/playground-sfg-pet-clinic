12a56654763299f0d64e3f99b2a15987ffed4a1e
dp <hudomokp@gmail.com>
2019-02-12 21:19:41 +0100

    Added vets list

--- sfg-pet-clinic-data/src/main/java/hu/atsoft/playground/sfgpetclinic/services/map/AbstractMapService.java
+++ sfg-pet-clinic-data/src/main/java/hu/atsoft/playground/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,13 +1,16 @@
 package hu.atsoft.playground.sfgpetclinic.services.map;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public abstract class AbstractMapService<T, ID> {
+import hu.atsoft.playground.sfgpetclinic.model.BaseEntity;
 
-	protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+	protected Map<Long, T> map = new HashMap<>();
 	
 	public Set<T> findAll() {
 		return new HashSet<>(map.values());
@@ -17,8 +20,15 @@ public abstract class AbstractMapService<T, ID> {
 		return map.get(id);
 	}
 	
-	public T save(ID id, T object) {
-		map.put(id, object);
+	public T save(T object) {
+		if (object!=null) {
+			if (object.getId()==null) {
+				object.setId(getNextId());
+			}
+		} else {
+			throw new RuntimeException("Object cannot be null");
+		}
+		map.put(object.getId(), object);
 		return object;
 	}
 	
@@ -28,5 +38,13 @@ public abstract class AbstractMapService<T, ID> {
 	
 	public void delete(T object) {
 		map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+	}
+	
+	private Long getNextId() {
+		if (!map.isEmpty()) {
+			return Collections.max(map.keySet()) + 1;
+		} else {
+			return 1L;
+		}
 	}
 }
