62914d1d0bb61d57c38461c734be7a4004ecf326
dp <hudomokp@gmail.com>
2019-02-17 10:25:10 +0100

    Added visit and owner relation to pet.

--- sfg-pet-clinic-data/src/main/java/hu/atsoft/playground/sfgpetclinic/model/Owner.java
+++ sfg-pet-clinic-data/src/main/java/hu/atsoft/playground/sfgpetclinic/model/Owner.java
@@ -1,6 +1,17 @@
 package hu.atsoft.playground.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
 	private static final long serialVersionUID = -7702850693393403030L;
 
+	private Set<Pet> pets;
+
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
 }
