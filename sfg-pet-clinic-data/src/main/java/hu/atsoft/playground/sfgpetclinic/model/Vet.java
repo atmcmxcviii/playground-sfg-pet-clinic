5a501add7dc0002c47e19d9094ba1d08f71c757a
dp <hudomokp@gmail.com>
2019-02-23 10:58:20 +0100

    Turned Vet into JPA entity

--- sfg-pet-clinic-data/src/main/java/hu/atsoft/playground/sfgpetclinic/model/Vet.java
+++ sfg-pet-clinic-data/src/main/java/hu/atsoft/playground/sfgpetclinic/model/Vet.java
@@ -1,11 +1,18 @@
 package hu.atsoft.playground.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 	private static final long serialVersionUID = -3762700820415926481L;
 
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "vet_specialties",
+		joinColumns = @JoinColumn(name = "vet_id"),
+		inverseJoinColumns = @JoinColumn(name = "specialty_id"))
 	private Set<Specialty> specialties = new HashSet<>();
 
 	public Set<Specialty> getSpecialties() {
