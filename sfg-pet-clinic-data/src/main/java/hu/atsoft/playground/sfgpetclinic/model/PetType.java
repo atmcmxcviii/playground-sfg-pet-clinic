91744d53b45566ca48b3339369028800f464a2f8
dp <hudomokp@gmail.com>
2019-02-23 10:47:38 +0100

    Turned Owner into JPA entity

--- sfg-pet-clinic-data/src/main/java/hu/atsoft/playground/sfgpetclinic/model/PetType.java
+++ sfg-pet-clinic-data/src/main/java/hu/atsoft/playground/sfgpetclinic/model/PetType.java
@@ -1,8 +1,15 @@
 package hu.atsoft.playground.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
 	private static final long serialVersionUID = -7765296113123373470L;
 	
+	@Column(name = "name")
 	private String name;
 
 	public String getName() {
